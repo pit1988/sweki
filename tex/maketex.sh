eba9a897b519ac6a513cdab88f572feac33bf596
gigiobello <giorgiogiuffre@tin.it>
2017-02-09 12:07:46 +0100

    travis prova

--- tex/maketex.sh
+++ tex/maketex.sh
@@ -38,6 +38,6 @@ rm -f _sweki_index
 echo "\end{document}" >> sweki.tex
 
 echo "generazione del documento PDF..."
-# due invocazioni del comando, per indice e riferimenti incrociati:
+# due invocazioni del comando, per riferimenti incrociati:
 pdflatex -halt-on-error sweki.tex > _sweki_log rm -f _sweki_log && pdflatex -halt-on-error sweki.tex > _sweki_log && rm -f _sweki_log && echo "File generato"
 rm -f *.log *.toc *.aux *.out *.tex # pulizia
