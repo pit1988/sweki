10fd4cb1a4b42793357d2c2fb187654ca5e84841
gigiobello <giorgiogiuffre@tin.it>
2017-02-09 10:40:52 +0100

    Updated .travis.yml

--- tex/maketex.sh
+++ tex/maketex.sh
@@ -2,7 +2,7 @@
 
 if [[ `basename \`pwd\`` != tex ]]; then
 	echo "devi essere nella directory tex/ per invocare questo script"
-	exit(1)
+	exit 1
 fi
 
 echo "\documentclass[a4paper]{article}" > sweki.tex
